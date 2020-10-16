7567781818f7b0d344b9c83e7b08dd106a78d509
Arhio <Arhio@yandex.ru>
2020-10-16 19:38:55 +0300

    Урок 2 - готов

---
+++ Lesson-2/Hubs/Client/Program.cs
@@ -0,0 +1,64 @@
+﻿using Common;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static HubConnection hubConnection;
+        static async Task Main(string[] args)
+        {
+            await InitSignalRConnaction();
+            bool isExit = false;
+
+            while(!isExit)
+            {
+
+                var input = Console.ReadLine();
+
+                if(!string.IsNullOrWhiteSpace(input))
+                {
+                    if (input == "exit")
+                        isExit = true;
+                    else if (input == "setname")
+                    {
+                        Console.WriteLine("write name");
+                        var name = "";
+
+                        while(string.IsNullOrWhiteSpace(name))                        
+                            name = Console.ReadLine();
+                        
+                        await hubConnection.SendAsync("SetName", name);
+
+
+                    }
+                    else
+                    {
+                        await hubConnection.SendAsync("SendMessage", new Message { Title = "Title", Body = input });
+                        Console.WriteLine("Send");
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        private static Task InitSignalRConnaction()
+        {
+            hubConnection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:50737/notify")
+                .Build();
+
+            hubConnection.On<Message>("Send", message => {
+                Console.WriteLine($"Message server Title: {message.Title} /n Body: {message.Body}");
+            });
+
+            return hubConnection.StartAsync();
+        }
+    }
+}
---
+++ Lesson-2/Hubs/Hubs/Controllers/NotificationController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common;
+using Hubs.Hubs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+
+namespace Hubs.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotificationController : ControllerBase
+    {     
+        public NotificationController()
+        {
+
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult<string>> Test()
+        {
+            return "Hello";
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Push([FromBody]Message message, [FromServices]IHubContext<NotificationHub, INotificationClient> hubContext)
+        {
+            await hubContext.Clients.All.Send(message);
+
+            return Ok();
+        }
+    }
+}
