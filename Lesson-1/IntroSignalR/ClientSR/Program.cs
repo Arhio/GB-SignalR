e104601de1983c3c0882464eee0514aa0699cc43
Arhio <Arhio@yandex.ru>
2020-10-16 15:24:14 +0300

    Урок 1 - Готов

---
+++ Lesson-1/IntroSignalR/ClientSR/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSR
+{
+    class Program
+    {
+        static HubConnection HubConnection;
+        static async Task Main(string[] args)
+        {
+            HubConnection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:50288/nitify")
+                .Build();
+
+            HubConnection.On<string>("Send", message => Console.WriteLine($"Message: {message}"));
+
+            await HubConnection.StartAsync();
+
+            bool isExit = false;
+
+            while(!isExit)
+            {
+                var message = Console.ReadLine();
+
+                if (message != "exit")
+                    await HubConnection.SendAsync("SendMessage", message);
+                else
+                    isExit = true;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
